fb0c64bb4255ece6ba15f3ce177e8b136fe121f1
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-03-19 21:40:55 -0500

    Create Clip.lua

---
+++ Clip.lua
@@ -0,0 +1,14 @@
+local noclip = true
+char = game.Players.LocalPlayer.Character
+while true do
+if noclip == true then
+for _,v in pairs(char:children()) do
+pcall(function()
+if v.className == "MeshPart" then
+v.CanCollide = true
+end
+end)
+end
+end
+game:service("RunService").Stepped:wait()
+end
