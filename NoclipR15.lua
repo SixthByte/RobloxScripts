8e294419a5eb5529e7f47b992e637582e59efae7
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-03-21 17:25:19 -0500

    Update NoclipR15.lua

--- NoclipR15.lua
+++ NoclipR15.lua
@@ -1,13 +1,6 @@
 Character = game.Players.LocalPlayer.Character
 game:GetService("RunService").Stepped:Connect(function()
-    Character.HumanoidRootPart.CanCollide = false
     Character.Head.CanCollide = false
     Character.UpperTorso.CanCollide = false
     Character.LowerTorso.CanCollide = false
-    Character.LeftUpperArm.CanCollide = false
-    Character.LeftLowerArm.CanCollide = false
-    Character.LeftHand.CanCollide = false
-    Character.RightUpperArm.CanCollide = false
-    Character.RightLowerArm.CanCollide = false
-    Character.RightHand.CanCollide = false
 end)
