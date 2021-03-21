4d1faece3402b88835cff313896083cbf196e87e
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-03-21 17:25:02 -0500

    Update ClipR15.lua

--- ClipR15.lua
+++ ClipR15.lua
@@ -1,13 +1,6 @@
 Character = game.Players.LocalPlayer.Character
 game:GetService("RunService").Stepped:Connect(function()
-    Character.HumanoidRootPart.CanCollide = false
     Character.Head.CanCollide = true
     Character.UpperTorso.CanCollide = true
     Character.LowerTorso.CanCollide = true
-    Character.LeftUpperArm.CanCollide = false
-    Character.LeftLowerArm.CanCollide = false
-    Character.LeftHand.CanCollide = false
-    Character.RightUpperArm.CanCollide = false
-    Character.RightLowerArm.CanCollide = false
-    Character.RightHand.CanCollide = false
 end)
