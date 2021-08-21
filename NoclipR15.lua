2821ea77a00659da7ac259129d3ef9f865e68270
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-08-21 11:20:38 -0500

    Delete NoclipR15.lua

--- NoclipR15.lua
+++
@@ -1,6 +0,0 @@
-Character = game.Players.LocalPlayer.Character
-game:GetService("RunService").Stepped:Connect(function()
-    Character.Head.CanCollide = false
-    Character.UpperTorso.CanCollide = false
-    Character.LowerTorso.CanCollide = false
-end)
