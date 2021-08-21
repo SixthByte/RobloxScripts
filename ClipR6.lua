990303e7bb71ccc343d180decf37d6bc181ba4d2
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-08-21 11:20:30 -0500

    Delete ClipR6.lua

--- ClipR6.lua
+++
@@ -1,5 +0,0 @@
-Character = game.Players.LocalPlayer.Character
-game:GetService("RunService").Stepped:Connect(function()
-    Character.Head.CanCollide = true
-    Character.Torso.CanCollide = true
-end)
