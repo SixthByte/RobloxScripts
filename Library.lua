c4a3800801ed6999c723e787d74c70d511c3ab4e
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-10-04 16:52:50 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -133,16 +133,18 @@ function Library:CreateMain(Options)
 		ScrollBarThickness = 0,
 	})
 	
-	local HexLogo = Instance.new("ImageLabel")
-	HexLogo.Name = "Logo"
-  	HexLogo.Parent = Main.Sidebar
-  	HexLogo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-  	HexLogo.BackgroundTransparency = 1.000
-  	HexLogo.Position = UDim2.new(0, 5, 0, 370)
-  	HexLogo.Size = UDim2.new(0, 40, 0, 40)
-  	HexLogo.ZIndex = 1
-  	HexLogo.Image = "rbxassetid://7403270296"
-  	HexLogo.ImageColor3 = Color3.fromRGB(255, 255, 255)
+	if _G.DisplayHexLogo then
+		local HexLogo = Instance.new("ImageLabel")
+		HexLogo.Name = "Logo"
+  		HexLogo.Parent = Main.Sidebar
+  		HexLogo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+  		HexLogo.BackgroundTransparency = 1.000
+  		HexLogo.Position = UDim2.new(0, 5, 0, 370)
+  		HexLogo.Size = UDim2.new(0, 40, 0, 40)
+  		HexLogo.ZIndex = 1
+  		HexLogo.Image = "rbxassetid://7403270296"
+  		HexLogo.ImageColor3 = Color3.fromRGB(255, 255, 255)
+	end
 
 	table.insert(Objects.GrayContrast, Main.Sidebar)
 
