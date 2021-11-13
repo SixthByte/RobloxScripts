8f7b87946f7c60014acf91faf3503df80758fabf
WolfGod746 <71614810+WolfGod746@users.noreply.github.com>
2021-11-13 16:32:09 -0600

    Update Hex.lua

--- Hex.lua
+++ Hex.lua
@@ -1,6 +1,8 @@
 local StartupGui = Instance.new("ScreenGui")
 local ImageFrame = Instance.new("Frame")
 local Image = Instance.new("ImageLabel")
+local Hex = Instance.new("ImageLabel")
+local Sound = Instance.new("Sound")
 
 StartupGui.Name = "StartupGui"
 StartupGui.Parent = game.CoreGui
@@ -20,25 +22,39 @@ Image.BackgroundTransparency = 1.000
 Image.Position = UDim2.new(-1.00641286, 0, -1.19648433, 0)
 Image.Size = UDim2.new(0, 300, 0, 300)
 Image.Image = "rbxassetid://7826686602"
+Image.ImageTransparency = 1
 
-wait(1.5)
+Hex.Name = "Hex"
+Hex.Parent = ImageFrame
+Hex.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Hex.BackgroundTransparency = 1.000
+Hex.Position = UDim2.new(-0.8, 0, -0.23, 0)
+Hex.Size = UDim2.new(0, 270, 0, 100)
+Hex.Image = "rbxassetid://7990079445"
+Hex.ImageTransparency = 1
 
-Image.ImageTransparency = 0.1
-wait(0.05)
-Image.ImageTransparency = 0.2
-wait(0.05)
-Image.ImageTransparency = 0.3
-wait(0.05)
-Image.ImageTransparency = 0.4
-wait(0.05)
-Image.ImageTransparency = 0.5
-wait(0.05)
-Image.ImageTransparency = 0.6
-wait(0.05)
-Image.ImageTransparency = 0.7
-wait(0.05)
-Image.ImageTransparency = 0.8
-wait(0.05)
-Image.ImageTransparency = 0.9
-wait(0.05)
+Sound.Name = "Windows11"
+Sound.Parent = ImageFrame
+Sound.SoundId = "rbxassetid://6958727243"
+Sound.TimePosition = 1
+Sound.Volume = 1.5
+Sound:Play()
+wait(0.1)
+for i = 1, 0.3, -0.05 do
+    Image.ImageTransparency = i
+    wait(0.01)
+end
+wait(0.3)
+for i = 0.03, 0, -0.5 do
+    Image.ImageTransparency = i
+    Hex.ImageTransparency = i
+    wait(0.01)
+end
+wait(1)
+
+for v = 0, 1, 0.15 do
+    Image.ImageTransparency = v
+    Hex.ImageTransparency = v
+    wait(0.01)
+end
 StartupGui:Destroy()
